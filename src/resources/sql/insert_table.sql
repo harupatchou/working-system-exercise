cb155b0dcab92b8b6f3b6c1d856735bec8895201
masashi.ueno <tashi480@gmail.com>
2016-03-03 11:38:36 +0900

    労働時間の受け渡しをintegerからobject型に変更

---
+++ src/resources/sql/insert_table.sql
@@ -0,0 +1,19 @@
+
+
+
+/*従業員情報*/
+INSERT INTO employee VALUES (1, '通常','従業員','rakus2000',1);
+INSERT INTO employee VALUES (2, '変形','従業員','rakus2000',2);
+INSERT INTO employee VALUES (3, 'フレックス','従業員','rakus2000',3);
+
+
+/*会社情報*/
+INSERT INTO company VALUES (1, '通常労働会社', 1);
+INSERT INTO company VALUES (2, '変形労働会社', 2);
+INSERT INTO company VALUES (3, 'フレックス制会社', 3);
+
+
+/*労働種別*/
+INSERT INTO workingtype VALUES (1, '通常労働制', 1);
+INSERT INTO workingtype VALUES (2, '変形労働制', 2);
+INSERT INTO workingtype VALUES (3, 'フレックス制', 3);
